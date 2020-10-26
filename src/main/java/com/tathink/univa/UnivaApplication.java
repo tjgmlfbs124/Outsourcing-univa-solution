40b3a5d9c39861248ed0bf66a31f0b43ef692c7b
qwanchoi <chlrbdhks92@gmail.com>
2020-10-26 10:50:53 +0900

    init project

---
+++ src/main/java/com/tathink/univa/UnivaApplication.java
@@ -0,0 +1,13 @@
+package com.tathink.univa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tathink/univa/UnivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tathink.univa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
